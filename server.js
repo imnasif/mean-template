54b7104a0d1ab877136591a1630a0f86bc3257bd
Nasif Ahmed <nasif.ahmed@infolytx.com>
2016-01-06 13:40:43 +0600

    Updated ports for Openshift deploy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express  = require('express');
 var app      = express();
 var mongoose = require('mongoose');
-var port  	 = process.env.PORT || 8080;
 var database = require('./config/database');
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+
 
 mongoose.connect(database.url);
 
@@ -17,7 +18,9 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 
+// routes ======================================================================
 require('./app/routes.js')(app);
 
+// listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
